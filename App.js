fcc9e4d21d21201ac03f4dfc91016871a6d1a224
NiceSlice <tanjamiskovic1@gmail.com>
2020-01-08 23:18:09 +0200

    good night

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button, Image, TouchableWithoutFeedback } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableWithoutFeedback, Animated, PanResponder } from 'react-native';
 
 const assets = {
   jelly: {
@@ -14,41 +14,37 @@ const assets = {
   }
 };
 
-const map = [[1, "", "", ""], ["", 2, "", ""], ["", "", 3, ""], ["", "", "", 4]];
+const map = [[1, "", "", ""], ["", 2, "", ""], ["", "", 3, ""], [1, "", "", 4]];
 
 class Field extends React.Component{
 
   state = {
-    pressed: false
+    flexValue: new Animated.Value(0.7),
+    selected: false//this.props.selected
+  }
+
+  growAnimation = () => {
+    Animated.timing(this.state.flexValue, {
+      toValue: 1,
+      duration: 500,
+    }).start();
   }
 
   render(){
 
     if(typeof(this.props.field) === "number"){
-
-      if(this.state.pressed){
-        return(
-
-          <TouchableWithoutFeedback onPress = {() => {this.setState({ pressed: true })}} >
-            <View style={styles.field} >
-              <Image style={[styles.jelly, styles.jellyPressed]} source={assets.jelly['shape' + this.props.field]} resizeMode="contain" ></Image>
-            </View>
-          </TouchableWithoutFeedback>
-
-        )
-      }
       
       return(
 
-        <TouchableWithoutFeedback onPress = {() => {this.setState({ pressed: true })}} >
-          <View style={styles.field} >
-            <Image style={styles.jelly} source={assets.jelly['shape' + this.props.field]} resizeMode="contain" ></Image>
-          </View>
-        </TouchableWithoutFeedback>
+        <View style={styles.field}>
+          <Animated.Image style={{flex: this.state.flexValue}} source={assets.jelly['shape' + this.props.field]} resizeMode="contain" ></Animated.Image>
+        </View>
 
       )
     }
 
+
+    //empty field
     return(
       
       <View style={styles.field}>
@@ -76,11 +72,68 @@ class Column extends React.Component{
 
 
 class Map extends Component{
+  state = {
+
+  }
+
+  constructor(props){
+    super(props);
+
+    this.state.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+
+      onPanResponderGrant: (evt, gestureState) => {
+
+        /*
+        x = evt.locationX / n - 1
+        y = evt.locationY / n - 1
+
+        field1 = (x, y)
+
+        if a numbered send a massage to field that it has been selected during the whole duration of touch
+        */
+
+      },
+      onPanResponderMove: (evt, gestureState) => {
+        
+        /*
+        x = evt.locationX / n - 1
+        y = evt.locationY / n - 1
+
+        if a numbered and if field1 is selected send a massage to field that it has been selected temporarily
+        */
+
+      },
+      onPanResponderTerminationRequest: (evt, gestureState) => true,
+      onPanResponderRelease: (evt, gestureState) => {
+        /*
+        x = evt.locationX / n - 1
+        y = evt.locationY / n - 1
+
+        field2 = (x, y)
+
+        if a numbered and if field1 is selected connect field1 and field2
+        well actually some additional rules will come with connecting like no going through jelly or bridges
+        and no connectiong jelly that's side by side or not in the same row/column
+
+        for now just get the selection process right
+        */
+      },
+      onPanResponderTerminate: (evt, gestureState) => {
+      },
+      onShouldBlockNativeResponder: (evt, gestureState) => {
+        return true;
+      },
+    });
+  }
 
   render(){
     return(
 
-      <View style={styles.map}>
+      <View style={styles.map} {...this.state.panResponder.panHandlers}>
         {this.props.map.map((column, index) =>  <Column column = {column} key = {index} />)}
       </View>
 
@@ -133,9 +186,12 @@ const styles = StyleSheet.create({
   },
   jelly: {
     flex: 0.7,
-  }
+  },
 });
 
 
-//think about the use of touchables and onPress
-//learn about how animations are made
+//I wrote some notes in the map's panresponder to work on today
+
+
+//
+//make jelly light up when it has correct number of connections
